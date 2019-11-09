c716ac3e4a681f0e72ba8ed3edd82b3471cd393b
jay-krishna <jayk674@gmail.com>
2019-11-09 19:51:34 +0530

    execute permissions & modified make file

--- alternate/create_nodes.py
+++ alternate/create_nodes.py
@@ -1,23 +1,27 @@
 import sys
 import shutil
 import os
+import glob
 
 arguments=int(sys.argv[1])
 
-file_lists=["node","node.o","nodeclass.o"]
+file_lists=["node","node.o","nodeclass.o","util.o","nodeserver.o"]
 
 for i in range(arguments):
 	folder_name="Node"+str(i)
 	
 	try:
-		shutil.rmtree(folder_name, ignore_errors=True)
+		for root, dirs, files in os.walk(folder_name):
+			for file in files:
+				os.remove(os.path.join(root, file))
 	except Exception:
-		print("",end="")
-
-	os.mkdir(folder_name)
+		os.mkdir(folder_name)
 
 	for file in file_lists:
 		src="ChordNode/"+file
 		des=folder_name+"/"+file
 
 		shutil.copyfile(src,des)
+
+	command="chmod +x "+folder_name+"/node"
+	os.system(command)
