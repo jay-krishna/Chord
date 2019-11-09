29bd57cf0bb3bed4962452606ba1052bcbb13a2b
pratikiiith <pratiktiwari94@gmail.com>
2019-11-09 17:17:18 +0530

    thread for successor

--- node.cpp
+++ node.cpp
@@ -14,6 +14,7 @@
 
 using namespace std;
 
+
 class Node{
 private:
 	string nodeip;
@@ -104,6 +105,12 @@ public:
 
 };
 
+struct threaddetails{
+	Node *currentnode;
+	int socketfd;
+};
+
+// creates a new connection given ip and portno
 int newconnection(string ip, string portno){
 	
 	long long int port = atoi(portno.c_str());
@@ -124,11 +131,68 @@ int newconnection(string ip, string portno){
 	return sockfd;
 }
 
-void *listener(void *fd){ // this function will create a node socket and will serve all request given by other nodes.
+
+// event function will handle all task given to listner
+void *event(void *fd){
+	struct threaddetails *nn = (struct threaddetails *)fd;
+
+	int clientsockfd = nn->socketfd;
+	Node *args = nn->currentnode;
+	
+	char buffer[1024];
+	memset(buffer,'\0',1024);
+	recv(clientsockfd,buffer,sizeof(buffer), 0);
+	string input;
+	int i=0;
+	while(buffer[i] != '\0'){
+		input = input + buffer[i];
+		i++;
+	}
+	cout << "command recv " << input << endl;
+	vector<string> command = splitcommand(input);
+	// recv command is "findsuccessor nodeid"
+	
+	if(command[0] == "findsuccessor"){
+		long long int rid = atoi(command[1].c_str());
+		long long int result = args->findsuccessor(rid);
+		if(result == -1){
+
+			pair<string,long long int> ipport = args->successordetail();
+			int newsockfd = newconnection(ipport.first,to_string(ipport.second));
+			string commandtosend = "findsuccessor " + to_string(rid); // command to be send to listner "findsuccessor nodeid"
+			char buffer[255];
+			memset(buffer,'\0',sizeof(buffer));
+			send(newsockfd,commandtosend.c_str(),commandtosend.size(),0);
+			recv(newsockfd,buffer,sizeof(buffer), 0);
+			string msg = "";
+			int j=0;
+			while(buffer[j] != '\0'){
+				msg = msg + buffer[j];
+				j++;
+			}
+			cout << "msg from neighbour " << msg << endl;
+			send(clientsockfd,msg.c_str(),msg.size(),0);
+		}
+		else{
+					
+			string r = to_string(result);
+			pair<string,long long int> otherdetails = args->successordetail();
+			r = r + " " + otherdetails.first + " " + to_string(otherdetails.second);
+			// send is "nodeid ip port"
+			send(clientsockfd,r.c_str(),r.size(),0);	
+		}
+				
+	}
+
+}
+
+// this function will create a node socket and will serve all request given by other nodes.
+void *listener(void *fd){ 
 		Node *args = (Node *)fd;
 		
 		string ip = args->getip();
 		long long int portno = args->getnodeportno();
+
 		cout << "Listner is on " << ip << " " << portno << endl;
 		int sockfd;
 		struct sockaddr_in serveraddress , clientaddress;
@@ -149,62 +213,25 @@ void *listener(void *fd){ // this function will create a node socket and will se
 			cout << "error bind socket" << endl;
 		}
 		listen(sockfd,5);
-		
+
+		int clientsockfd[100];
+		int sockcount=0;
+
 		while(1)
 		{
 			socklen_t clilen = sizeof(clientaddress);
-			int clientsockfd = accept(sockfd,(struct sockaddr *)&clientaddress,&clilen);
-			cout << "clientsockfd " << clientsockfd << endl;
+			clientsockfd[sockcount] = accept(sockfd,(struct sockaddr *)&clientaddress,&clilen);
 			//do all the task given to listener
-			
-			char buffer[1024];
-			memset(buffer,'\0',1024);
-			recv(clientsockfd,buffer,sizeof(buffer), 0);
-			string input;
-			int i=0;
-			while(buffer[i] != '\0'){
-				input = input + buffer[i];
-				i++;
-			}
-			cout << "command recv " << input << endl;
-			vector<string> command = splitcommand(input);
-			// recv command is "findsuccessor nodeid"
-			if(command[0] == "findsuccessor"){
-
-				long long int rid = atoi(command[1].c_str());
-				long long int result = args->findsuccessor(rid);
-				if(result == -1){
-
-						pair<string,long long int> ipport = args->successordetail();
-						int newsockfd = newconnection(ipport.first,to_string(ipport.second));
-						string commandtosend = "findsuccessor " + to_string(rid); // command to be send to listner "findsuccessor nodeid"
-						char buffer[255];
-						memset(buffer,'\0',sizeof(buffer));
-						send(newsockfd,commandtosend.c_str(),commandtosend.size(),0);
-						recv(newsockfd,buffer,sizeof(buffer), 0);
-						string msg = "";
-						int j=0;
-						while(buffer[j] != '\0'){
-							msg = msg + buffer[j];
-							j++;
-						}
-						cout << "msg from neighbour " << msg << endl;
-						send(clientsockfd,msg.c_str(),msg.size(),0);
-
-				}
-				else{
-					
-					string r = to_string(result);
-					pair<string,long long int> otherdetails = args->successordetail();
-					r = r + " " + otherdetails.first + " " + to_string(otherdetails.second);
-					// send is "nodeid ip port"
-					send(clientsockfd,r.c_str(),r.size(),0);	
-				}
-				
-			}
-
+			struct threaddetails td;
+			td.currentnode = args;
+			td.socketfd = clientsockfd[sockcount];
+			pthread_t th;
+			pthread_create(&th,NULL,event,(void *)&td);
+			pthread_detach(th);
+			sockcount++;
 		}
 }
+
 
 int main()
 {
@@ -317,6 +344,7 @@ int main()
 			string idpp = command[3];
 			currentnode.successor(command[1],atoi(ppp.c_str()),atoi(idpp.c_str()));
 		}
+
 		else{
 			cout << "Wrong Command input" << endl;
 		}
