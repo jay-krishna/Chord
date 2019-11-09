1da60615bc37acb25fbc843a6f073b2d8d77a895
jay-krishna <jayk674@gmail.com>
2019-11-10 01:46:33 +0530

    automate

--- alternate/create_nodes.py
+++ alternate/create_nodes.py
@@ -9,11 +9,20 @@ file_lists=["node","node.o","nodeclass.o","util.o","nodeserver.o"]
 
 for _ in os.listdir("."):
 	if(re.search("^Node[0-9]+$",_)):
-		shutil.rmtree(os.path.join(os.getcwd(),_))
+		num=int(_.split("Node")[1])
+		if(num>=arguments):
+			shutil.rmtree(_)
+			continue
+
+		for root, dirs, files in os.walk(_, topdown=False):
+			for name in files:
+				os.remove(os.path.join(root, name))
+
 
 for i in range(arguments):
 	folder_name="Node"+str(i)
-	os.mkdir(folder_name)
+	if not (os.path.isdir(os.path.join(os.getcwd(),folder_name))):
+		os.mkdir(folder_name)
 
 	for file in file_lists:
 		src="ChordNode/"+file
